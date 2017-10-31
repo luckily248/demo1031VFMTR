c2f75ffacd318e3c55943edbc2a51569ad38ab7d
luckily248 <luck248@163.com>
2017-10-31 11:51:34 -0400

    Initial Commit

---
+++ demo1031VFMTR/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  demo1031VFMTR
+//
+//  Created by lucky on 10/31/17.
+//  Copyright © 2017 luck. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
